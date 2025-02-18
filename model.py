2541e8c96f5cd4bca6996eeb67dc6e4d5280b6b5
roddms <kke3007@sookmyung.ac.kr>
2025-02-18 16:49:05 +0900

    commit

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.models import load_model  # TensorFlow is required for Keras to work
+from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 
